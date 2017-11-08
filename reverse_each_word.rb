a5e5116236ce65fddfae089bfae51a21b966476c
Neena van de Water <nkuriakose@hotmail.com>
2017-11-08 21:05:44 +0000

    automatically backed up by learn

--- reverse_each_word.rb
+++ reverse_each_word.rb
@@ -10,9 +10,9 @@
 
 def reverse_each_word(sentence)
   new_array = sentence.split(" ")
-  reversed_sentence = []
+  
   new_array.collect do | word |
-    reversed_sentence << word.reverse
+    word.reverse
   end
   return reversed_sentence.join(" ")
 end
